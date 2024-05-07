d1315cb2edf71552bb30772ae7624a458c80c6b2
Yuma Ishikawa <yuma.fuu05@gmail.com>
2024-05-08 03:56:16 +0900

    [update] bufferline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -376,39 +376,36 @@ return {
     config = function()
       require('bufferline').setup {
         options = {
-          separator_style = { '│', '│' },
+          separator_style = { '', '' },
           always_show_bufferline = true,
           show_buffer_close_icons = false,
           show_close_icon = false,
           show_tab_indicators = false,
-          color_icons = false,
+          color_icons = true,
           indicator = {
             style = 'none',
           },
-          offsets = {
-            {
-              filetype = "NvimTree",
-              text = "File Explorer",
-              text_align = "left",
-              separator = false,
-            },
-          },
+          -- offsets = {
+          --   {
+          --     filetype = "NvimTree",
+          --     text = "File Explorer",
+          --     text_align = "left",
+          --     separator = false,
+          --   },
+          -- },
         },
         highlights = {
-          separator = {
-            guifg = '#073642',
-            guibg = '#002b36',
-          },
-          separator_selected = {
-            guifg = '#073642',
-          },
           background = {
-            guifg = '#657b83',
-            guibg = '#303030'
+            fg = '#D0D0D0',
+            bg = '#303030',
           },
           buffer_selected = {
-            guifg = '#fdf6e3',
-            gui = "bold",
+            fg = '#7CB0FF',
+            bold = true,
+          },
+          fill = {
+            bg = '#303030',
+            fg = "#D0D0D0",
           },
         },
       }
