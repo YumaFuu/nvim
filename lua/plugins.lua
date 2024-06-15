9f92f6f65369d99663072546bc41effb7c5d0d3f
Yuma Ishikawa <yuma.fuu05@gmail.com>
2024-06-15 14:37:51 +0900

    [add] cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,6 +3,15 @@ return {
   'MunifTanjim/nui.nvim',
   'RRethy/vim-illuminate',
   {
+    'TobinPalmer/rayso.nvim',
+    cmd = { 'Rayso' },
+    config = function()
+      require('rayso').setup {
+        open_cmd = 'open-via-lemonade',
+      }
+    end
+  },
+  {
     'rapan931/lasterisk.nvim',
     config = function()
       vim.keymap.set('n', '*', function()
@@ -346,6 +355,7 @@ return {
       require("mason").setup()
     end,
   },
+  'mattn/vim-goimports',
   {
     'neovim/nvim-lspconfig',
   },
@@ -374,11 +384,100 @@ return {
       vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
       vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
       vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
-      vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-      vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+      vim.keymap.set('n', 'K',  '<cmd>lua vim.lsp.buf.hover()<CR>')
+      vim.keymap.set('n', 'ga', '<cmd>lua vim.diagnostic.open_float()<CR>')
       vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
       vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
     end
+  },
+  'neovim/nvim-lspconfig',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  {
+    'hrsh7th/nvim-cmp',
+    config = function()
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        {
+          border = "rounded", -- "shadow" , "none", "rounded"
+          -- border = border
+          -- width = 100,
+        }
+      )
+      local cmp = require'cmp'
+
+      cmp.setup({
+        snippet = {
+          -- REQUIRED - you must specify a snippet engine
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+          end,
+        },
+        window = {
+          -- completion = cmp.config.window.bordered(),
+          -- documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' }, -- For vsnip users.
+          -- { name = 'luasnip' }, -- For luasnip users.
+          -- { name = 'ultisnips' }, -- For ultisnips users.
+          -- { name = 'snippy' }, -- For snippy users.
+        }, {
+            { name = 'buffer' },
+          })
+      })
+
+      -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+      -- Set configuration for specific filetype.
+      --[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' },
+    }, {
+      { name = 'buffer' },
+    })
+ })
+ require("cmp_git").setup() ]]--
+
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+            { name = 'cmdline' }
+          }),
+        matching = { disallow_symbol_nonprefix_matching = false }
+      })
+
+      -- Set up lspconfig.
+      -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+      require('lspconfig')['gopls'].setup {
+        capabilities = capabilities
+      }
+    end,
   },
   {
     "shellRaining/hlchunk.nvim",
@@ -567,9 +666,11 @@ return {
             vim.api.nvim_buf_get_name(props.buf),
             ':t'
           )
+
           if filename == '' then
-            filename = '[No Name]'
+            filename = vim.bo[props.buf]
           end
+
           -- local ft_icon, ft_color = devicons.get_icon_color(filename)
           local modified = vim.bo[props.buf].modified
           return {
